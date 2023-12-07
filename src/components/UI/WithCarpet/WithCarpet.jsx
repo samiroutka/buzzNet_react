c2e047826cc5fcd946e54dd4f26df488bbb84032
samiro <sarkisovmichail007@gmail.com>
2023-12-07 15:16:27 +0300

    carpet & minor edits 2

---
+++ src/components/UI/WithCarpet/WithCarpet.jsx
@@ -0,0 +1,30 @@
+import React, { cloneElement, useRef } from 'react'
+import styles from './WithCarpet.module.scss'
+import { InView, useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
+
+export const WithCarpet = ({children, activeClass}) => {
+  let carpetRef = useRef()
+  let childrenRef = useRef()
+  !children.ref ? children = cloneElement(children, {ref: childrenRef}) : false
+  let observer = new IntersectionObserver((entries) => {
+    // entries - это все элементы на которые повешан данный observer
+    if (entries[0].isIntersecting) {
+      carpetRef.current.classList.add(styles.carpet_active)
+    }
+  }, {threshold: 0});
+  useEffect(() => {
+    observer.observe(children.ref.current)
+  }, [])
+
+  // --------------------------------------
+  return (
+    <div>
+      {children}
+      <div ref={carpetRef} className={`${styles.carpet}`} onClick={() => {
+        children.ref.current.classList.remove(activeClass)
+        carpetRef.current.classList.remove(styles.carpet_active)
+      }}></div>
+    </div>
+  )
+}
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -9,51 +9,15 @@ import { CircularProgress, IconButton } from '@mui/material';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { Button, TextField } from '@mui/material';
 import { useRef } from 'react';
-import { rememberUser } from '@/utils'
+import { rememberUser, jsonParseData } from '@/utils'
 import { useState } from 'react';
+import { WithCarpet } from '@/components/UI/WithCarpet/WithCarpet.jsx';
 
 const UserPage = React.forwardRef((props, ref) => {
   let apiUrl = import.meta.env.VITE_APIURL
   let navigateTo = useNavigate()
   let {userData, setUserData} = useContext(Context)
   let settingsRef = useRef()
-  let carpetRef = useRef()
-  let fieldNameRef = useRef()
-  let fieldPasswordRef = useRef()
-  let avatarInputSettingsRef = useRef()
-  let [isSettingsLoading, setIsSettingsLoading] = useState(false)
-  let [settingsNameError, setSettingsNameError] = useState(false)
-  let [settingsPasswordError, setSettingsPasswordError] = useState(false)
-  let [settingsAvatarUrl, setSettingsAvatarUrl] = useState()
-
-  useEffect(() => {
-    setSettingsAvatarUrl(apiUrl + userData.avatar)
-  }, [userData])
-
-  let updateData = async () => {
-    let formData = new FormData()
-    formData.append('name', fieldNameRef.current.querySelector('input').value)
-    formData.append('password', fieldPasswordRef.current.querySelector('input').value)
-    formData.append('avatar', avatarInputSettingsRef.current.files[0])
-    let response = await fetch(`${apiUrl}user/${userData.name}`, {
-      method: 'PUT',
-      body: formData
-    })
-    response = await response.json()
-    if (response.name == fieldNameRef.current.querySelector('input').value) {
-      response.posts = JSON.parse(response.posts)
-      setUserData(response)
-      rememberUser(document, response.name, response.password)
-    } else {
-      console.log(response)
-    }
-  }
-
-  let quitAccount = async () => {
-    document.cookie = 'name=0;max-age=0'
-    document.cookie = 'password=0;max-age=0'
-    location.reload()
-  }
 
   let addPost = async () => {
     let response = await fetch(`${apiUrl}user/${userData.name}/posts`, {
@@ -65,53 +29,109 @@ const UserPage = React.forwardRef((props, ref) => {
     navigateTo(`/posts/${newPost.id}`)
   }
 
-  let container_subscribers = useRef()
-  let container_subscriptions = useRef()
+  let dataListSubscribersRef = useRef()
+  let dataListSubscriptionsRef = useRef()
   let showList = (type) => {
     // subscribers/subscriptions
-    type == 'subscribers' ? container_subscribers.current.classList.add(styles.data__listContainer_active)
-    : container_subscriptions.current.classList.add(styles.data__listContainer_active)
+    type == 'subscribers' ? dataListSubscribersRef.current.classList.add(styles.data__list_active)
+    : dataListSubscriptionsRef.current.classList.add(styles.data__list_active)
   }
 
-  // --------------------
+  // -----------------Components---------------------
+
+  let [isSettingsOpen, setIsSettingsOpen] = useState(false)
+  let UserSettings = React.forwardRef((props, ref) => {
+    let [settingsAvatarUrl, setSettingsAvatarUrl] = useState()
+    let [settingsNameError, setSettingsNameError] = useState(false)
+    let [settingsPasswordError, setSettingsPasswordError] = useState(false)
+    let [isSettingsLoading, setIsSettingsLoading] = useState(false)
+    let fieldNameRef = useRef()
+    let fieldPasswordRef = useRef()
+    let avatarInputSettingsRef = useRef()
+
+    useEffect(() => {
+      console.log(isSettingsOpen)
+    }, [])
+
+    useEffect(() => {
+      setSettingsAvatarUrl(apiUrl + userData.avatar)
+    }, [userData])
+
+    let quitAccount = async () => {
+      document.cookie = 'name=0;max-age=0'
+      document.cookie = 'password=0;max-age=0'
+      location.reload()
+    }
+
+    let resetErrors = () => {
+      setSettingsNameError(false)
+      setSettingsPasswordError(false)
+    }
+
+    let validate = (maxLength = 6) => {
+      let inputName = fieldNameRef.current.querySelector('input').value
+      let inputPassword = fieldPasswordRef.current.querySelector('input').value
+      inputName.length < maxLength ? setSettingsNameError(`Минимальное кол-во символов: ${maxLength}`) : false
+      inputPassword.length < maxLength ? setSettingsPasswordError(`Минимальное кол-во символов: ${maxLength}`) : false
+      if (inputName.length >= maxLength && inputPassword.length >= maxLength) {
+        return true
+      } else {
+        return false
+      }
+    } 
+
+    let updateData = async () => {
+      let formData = new FormData()
+      formData.append('name', fieldNameRef.current.querySelector('input').value)
+      formData.append('password', fieldPasswordRef.current.querySelector('input').value)
+      formData.append('avatar', avatarInputSettingsRef.current.files[0])
+      let response = await fetch(`${apiUrl}user/${userData.name}`, {
+        method: 'PUT',
+        body: formData
+      })
+      if (response.ok) {
+        response = await response.json()
+        setIsSettingsOpen(true)
+        setUserData(jsonParseData(response))
+        rememberUser(document, response.name, response.password)
+      } else {
+        console.log('updateData error')
+      }
+    }
+
+    // ------------
+    return (
+      <div ref={settingsRef} className={`${styles.settings} ${isSettingsOpen ? styles.settings_active : false}`}>
+        {isSettingsLoading ? <CircularProgress className={styles.settings__loader}/> : <></>}
+        <input ref={avatarInputSettingsRef} onChange={event => {
+          setSettingsAvatarUrl(URL.createObjectURL(event.target.files[0]))
+        }} hidden accept="image/*" type="file" id='settingsAvatar'/>
+        <label htmlFor="settingsAvatar"><Avatar className={styles.settings__avatar} src={settingsAvatarUrl}/></label>
+        <TextField ref={fieldNameRef} error={Boolean(settingsNameError)} helperText={settingsNameError} label='Имя' defaultValue={userData.name}></TextField>
+        <TextField ref={fieldPasswordRef} error={Boolean(settingsPasswordError)} helperText={settingsPasswordError} label='Пароль' defaultValue={userData.password}></TextField>
+        <Button onClick={async () => {
+          setIsSettingsLoading(true)
+          resetErrors()
+          validate() ? await updateData() : false
+          setIsSettingsLoading(false)
+        }} className={styles.settings__saveButton} variant='contained'>сохранить</Button>
+        <Button className={styles.settings__quitButton} onClick={quitAccount}>Выйти из аккаунта</Button>
+      </div>
+    )
+  })
+
+
+  // ------------------------------------------------
   return (
     <>
       {!userData ? <MyLoader/> :
-        <div className={styles.usePage} {...props} ref={ref}>
-          <div ref={settingsRef} className={styles.settings}>
-            {isSettingsLoading ? <CircularProgress className={styles.settings__loader}/> : <></>}
-            <input ref={avatarInputSettingsRef} onChange={event => {
-              setSettingsAvatarUrl(URL.createObjectURL(event.target.files[0]))
-            }} hidden accept="image/*" type="file" id='settingAvatar'/>
-            <label htmlFor="settingAvatar"><Avatar className={styles.settings__avatar} src={settingsAvatarUrl}/></label>
-            <TextField ref={fieldNameRef} error={Boolean(settingsNameError)} helperText={settingsNameError} label='Имя' defaultValue={userData.name}/>
-            <TextField ref={fieldPasswordRef} error={Boolean(settingsPasswordError)} helperText={settingsPasswordError} label='Пароль' defaultValue={userData.password}/>
-            <Button onClick={async () => {
-              setIsSettingsLoading(true)
-              setSettingsNameError(false)
-              setSettingsPasswordError(false)
-              let inputNameLength = fieldNameRef.current.querySelector('input').value.length
-              let inputPasswordLength = fieldPasswordRef.current.querySelector('input').value.length
-              inputNameLength < 6 ? setSettingsNameError('Недостоточно символов (<6)') : false
-              inputPasswordLength < 6 ? setSettingsPasswordError('Недостоточно символов (<6)') : false
-              if (inputNameLength >= 6 && inputPasswordLength >= 6){
-                setSettingsNameError(false)
-                setSettingsPasswordError(false)
-                await updateData()
-              }
-              setIsSettingsLoading(false)
-            }} className={styles.settings__saveButton} variant='contained'>сохранить</Button>
-            <Button className={styles.settings__quitButton} onClick={quitAccount}>Выйти из аккаунта</Button>
-          </div>
-          <div ref={carpetRef} className={styles.carpet}></div>
+        <div className={styles.userPage} {...props} ref={ref}>
+          <WithCarpet activeClass={styles.settings_active}>
+            <UserSettings ref={settingsRef}/>
+          </WithCarpet>
           <header>
             <IconButton className={styles.settings__button} onClick={() => {
-              settingsRef.current.classList.add(styles.opened)
-              carpetRef.current.classList.add(styles.active)
-              carpetRef.current.addEventListener('click', () => {
-                settingsRef.current.classList.remove(styles.opened)
-                carpetRef.current.classList.remove(styles.active)
-              })
+              settingsRef.current.classList.toggle(styles.settings_active)
             }}>
               <SettingsIcon/>
             </IconButton>
@@ -126,32 +146,32 @@ const UserPage = React.forwardRef((props, ref) => {
                 <strong>Посты</strong>
                 <span>{userData.posts.length}</span>
               </p>
-              <p className={styles.data__block} onClickCapture={event => showList('subscribers')}>
+              <p className={styles.data__block} onClick={() => showList('subscribers')}>
                 <strong>Подписчики</strong>
                 <span>{userData.subscribers.length}</span>
               </p>
-              <div ref={container_subscribers} className={styles.data__listContainer}>
-                <div className={styles.data__list}>
+              <WithCarpet activeClass={styles.data__list_active}>
+                <div ref={dataListSubscribersRef} className={styles.data__list}>
                   {userData.subscribers.map(subscriber =>
                     <div className={styles.data__listItem} key={subscriber}>
-                      <p>{subscriber}</p>
-                    </div>)}
+                      <a href={`${location.href}users/${subscriber}`}>{subscriber}</a>
+                    </div>)
+                  }
                 </div>
-                <div className={styles.carpet_test} onClick={() => container_subscribers.current.classList.remove(styles.data__listContainer_active)}></div>
-              </div>
+              </WithCarpet>
               <p className={styles.data__block} onClick={() => showList('subscriptions')}>
                 <strong>Подписки</strong>
                 <span>{userData.subscriptions.length}</span>
               </p>
-              <div ref={container_subscriptions} className={styles.data__listContainer}>
-                <div className={styles.data__list}>
+              <WithCarpet activeClass={styles.data__list_active}>
+                <div ref={dataListSubscriptionsRef} className={styles.data__list}>
                   {userData.subscriptions.map(subscription =>
                     <div className={styles.data__listItem} key={subscription}>
-                      <p>{subscription}</p>
-                    </div>)}
+                      <a href={`${location.href}users/${subscription}`}>{subscription}</a>
+                    </div>)
+                  }
                 </div>
-                <div className={styles.carpet_test} onClick={() => container_subscriptions.current.classList.remove(styles.data__listContainer_active)}></div>
-              </div>
+              </WithCarpet>
             </div>
             <div className={styles.posts}>
               {userData.posts.map(post => 
