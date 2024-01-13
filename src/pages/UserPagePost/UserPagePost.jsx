51b9d266d5a2143a72d1d107b597119e84b7b249
samiro <sarkisovmichail007@gmail.com>
2024-01-13 22:59:07 +0300

    advanced editor

--- src/pages/UserPagePost/UserPagePost.jsx
+++ src/pages/UserPagePost/UserPagePost.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useRef, useState } from 'react'
 import { useParams, useNavigate } from 'react-router'
 import styles from './UserPagePost.module.scss'
 import { Context } from '../../context'
-import { TextField, Button } from '@mui/material'
+import { TextField, Button, CircularProgress, Alert } from '@mui/material'
 import MyLoader from '../../components/UI/MyLoader/MyLoader'
 import { Editor } from '@tinymce/tinymce-react';
 import SaveIcon from '@mui/icons-material/Save';
@@ -17,6 +17,8 @@ const UserPagePost = () => {
   let [isLoading, setIsLoading] = useState(true)
   let editorRef = useRef()
   let post = userData ? userData.posts.find(post => post.id == postId) : false
+  let [mediaLoader, setMediaLoader] = useState(false)
+  let [editorAlert, setEditorAlert] = useState(false)
 
   let savePost = async () => {
     let formData = new FormData()
@@ -60,12 +62,41 @@ const UserPagePost = () => {
           <div className={styles.post}>
             {isLoading ? <MyLoader/> : <></>}
             <TextField ref={titleElement} className={styles.title} defaultValue={post.title} id="standard-basic" label="Название поста" variant="standard"/>
+            {mediaLoader ? <div className={styles.mediaLoader}><CircularProgress/></div> : null}
+            {editorAlert ? <Alert className={styles.editorAlert} severity="info" onClose={() => {
+              setEditorAlert(false)
+            }}>Файл превышает 100 МБ</Alert> : null}
             <Editor initialValue={post.content} ref={editorRef} onInit={() => {
               setIsLoading(false)
-            }} init={{
-                plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
-                toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
-            }}/>
+              }} init={{
+                menubar: false,
+                toolbar: 'undo redo | fontsize fontfamily bold italic | alignleft aligncenter alignright alignjustify | image media preview',
+                // plugins: 'image media preview imagetools searchreplace',
+                plugins: 'advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table paste imagetools wordcount',
+                file_picker_types: 'image media',
+                file_picker_callback: (cb, value, meta) => {
+                  let file_input = document.createElement('input')
+                  file_input.setAttribute('type', 'file')
+                  file_input.setAttribute('accept', meta.filetype == 'image' ? 'image/*' : 'video/*')
+                  file_input.onchange = async () => {
+                    if (file_input.files[0].size/1048576 <= 100) {
+                      setMediaLoader(true)
+                      let formData = new FormData()
+                      formData.append('media', file_input.files[0])
+                      let response = await fetch(`${apiUrl}${meta.filetype == 'image' ? 'mediadowloading/images/' : 'mediadowloading/videos/'}`, {
+                        method: 'post',
+                        body: formData
+                      })
+                      cb(`${apiUrl}${await response.json()}`)
+                      setMediaLoader(false)
+                    } else {
+                      setEditorAlert(true)
+                    }
+                  }
+                  file_input.click()
+                }
+              }}
+            />
             <Button className={styles.saveButton} onClick={async () => {
               setIsLoading(true)
               await savePost()
